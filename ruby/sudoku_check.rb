58d85ce4d7eec1b583797b2edd2cbc20c2d37825
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2018-02-09 21:09:17 +0100

    removed test

--- ruby/sudoku_check.rb
+++ ruby/sudoku_check.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def done_or_not(board)
   return "Try again!" unless check_rows(board)
   return "Try again!" unless check_columns(board)
@@ -31,13 +29,3 @@ def check_sections(board)
   end
   true
 end
-
-p check_sections([[5, 3, 4, 6, 7, 8, 9, 1, 0],
-                         [6, 7, 2, 1, 9, 5, 3, 4, 8],
-                         [1, 9, 8, 3, 4, 2, 5, 6, 7],
-                         [8, 5, 9, 7, 6, 1, 4, 2, 3],
-                         [4, 2, 6, 8, 5, 3, 7, 9, 1],
-                         [7, 1, 3, 9, 2, 4, 8, 5, 6],
-                         [9, 6, 1, 5, 3, 7, 2, 8, 4],
-                         [2, 8, 7, 4, 1, 9, 6, 3, 5],
-                         [3, 4, 5, 2, 8, 6, 1, 7, 9]])
